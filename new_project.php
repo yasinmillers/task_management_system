fc2094859188070dfb7f733864a1d72f9d69fe39
yasinmillers <ssenyonga100@gmail.com>
2022-04-07 08:27:09 +0300

    names

--- new_project.php
+++ new_project.php
@@ -17,9 +17,9 @@
 				<div class="form-group">
 					<label for="">Status</label>
 					<select name="status" id="status" class="custom-select custom-select-sm">
-						<option value="0" <?php echo isset($status) && $status == 0 ? 'selected' : '' ?>>Pending</option>
-						<option value="3" <?php echo isset($status) && $status == 3 ? 'selected' : '' ?>>On-Hold</option>
-						<option value="5" <?php echo isset($status) && $status == 5 ? 'selected' : '' ?>>Done</option>
+						<option value="0" <?php echo isset($status) && $status == 0 ? 'selected' : '' ?>>Idea</option>
+						<option value="3" <?php echo isset($status) && $status == 3 ? 'selected' : '' ?>>Proposal</option>
+						<option value="5" <?php echo isset($status) && $status == 5 ? 'selected' : '' ?>>Report</option>
 					</select>
 				</div>
 			</div>
