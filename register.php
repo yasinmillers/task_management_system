8272df9d0c639799cf483222fbbdf77433104eaf
yasinmillers <ssenyonga100@gmail.com>
2022-04-07 13:23:10 +0300

    new

--- register.php
+++ register.php
@@ -1,99 +1,137 @@
 <?php
-require_once "db_connect.php";
-if (isset($_POST['signup'])) {
-$name = mysqli_real_escape_string($conn, $_POST['name']);
-$regno = mysqli_real_escape_string($conn, $_POST['regno']);
-$program = mysqli_real_escape_string($conn, $_POST['program']);
-$email = mysqli_real_escape_string($conn, $_POST['email']);
-$mobile = mysqli_real_escape_string($conn, $_POST['mobile']);
-$password = mysqli_real_escape_string($conn, $_POST['password']);
-$cpassword = mysqli_real_escape_string($conn, $_POST['cpassword']); 
-
-if (!preg_match("/^[a-zA-Z ]+$/",$name)) {
-$name_error = "Name must contain only alphabets and space";
-}
-if (!preg_match("/[^A-Za-z0-9]+/",$regno)) {
-    $regno_error = "contain only alphabets and number";
-}
-if (!preg_match("/^[a-zA-Z ]+$/",$program)) {
-    $program_error = "program must contain only alphabets and space";
-}
-if(!filter_var($email,FILTER_VALIDATE_EMAIL)) {
-$email_error = "Please Enter Valid Email ID";
-}
-if(strlen($password) < 6) {
-$password_error = "Password must be minimum of 6 characters";
-}       
-if(strlen($mobile) < 10) {
-$mobile_error = "Mobile number must be minimum of 10 characters";
-}
-if($password != $cpassword) {
-$cpassword_error = "Password and Confirm Password doesn't match";
-}
- 
-$query = "INSERT INTO students(name,regno,program, email, mobile ,password) VALUES('" . $name . "', '" . $regno . "','" . $program . "','" . $email . "', '" . $mobile . "', '" . md5($password) . "')"
-
-
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<title>register student</title>
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
+<div class="col-lg-12">
+	<div class="card">
+		<div class="card-body">
+			<form action="" id="manage_user">
+				<input type="hidden" name="id" value="<?php echo isset($id) ? $id : '' ?>">
+				<div class="row">
+					<div class="col-md-6 border-right">
+						<div class="form-group">
+							<label for="" class="control-label">First Name</label>
+							<input type="text" name="firstname" class="form-control form-control-sm" required value="<?php echo isset($firstname) ? $firstname : '' ?>">
+						</div>
+						<div class="form-group">
+							<label for="" class="control-label">Last Name</label>
+							<input type="text" name="lastname" class="form-control form-control-sm" required value="<?php echo isset($lastname) ? $lastname : '' ?>">
+						</div>
+						<?php if($_SESSION['login_type'] == 1): ?>
+						<div class="form-group">
+							<label for="" class="control-label">User Role</label>
+							<select name="type" id="type" class="custom-select custom-select-sm">
+								<option value="3" <?php echo isset($type) && $type == 3 ? 'selected' : '' ?>>vistor</option>
+								<option value="2" <?php echo isset($type) && $type == 2 ? 'selected' : '' ?>>Project Supervisor</option>
+								<option value="1" <?php echo isset($type) && $type == 1 ? 'selected' : '' ?>>student</option>
+							</select>
+						</div>
+						<?php else: ?>
+							<input type="hidden" name="type" value="3">
+						<?php endif; ?>
+						<div class="form-group">
+							<label for="" class="control-label">Avatar</label>
+							<div class="custom-file">
+		                      <input type="file" class="custom-file-input" id="customFile" name="img" onchange="displayImg(this,$(this))">
+		                      <label class="custom-file-label" for="customFile">Choose file</label>
+		                    </div>
+						</div>
+						<div class="form-group d-flex justify-content-center align-items-center">
+							<img src="<?php echo isset($avatar) ? 'assets/uploads/'.$avatar :'' ?>" alt="Avatar" id="cimg" class="img-fluid img-thumbnail ">
+						</div>
+					</div>
+					<div class="col-md-6">
+						
+						<div class="form-group">
+							<label class="control-label">Email</label>
+							<input type="email" class="form-control form-control-sm" name="email" required value="<?php echo isset($email) ? $email : '' ?>">
+							<small id="#msg"></small>
+						</div>
+						<div class="form-group">
+							<label class="control-label">Password</label>
+							<input type="password" class="form-control form-control-sm" name="password" <?php echo !isset($id) ? "required":'' ?>>
+							<small><i><?php echo isset($id) ? "Leave this blank if you dont want to change you password":'' ?></i></small>
+						</div>
+						<div class="form-group">
+							<label class="label control-label">Confirm Password</label>
+							<input type="password" class="form-control form-control-sm" name="cpass" <?php echo !isset($id) ? 'required' : '' ?>>
+							<small id="pass_match" data-status=''></small>
+						</div>
+					</div>
+				</div>
+				<hr>
+				<div class="col-lg-12 text-right justify-content-center d-flex">
+					<button class="btn btn-primary mr-2">Save</button>
+					<button class="btn btn-secondary" type="button" onclick="location.href = 'index.php?page=user_list'">Cancel</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+<style>
+	img#cimg{
+		height: 15vh;
+		width: 15vh;
+		object-fit: cover;
+		border-radius: 100% 100%;
+	}
+</style>
+<script>
+	$('[name="password"],[name="cpass"]').keyup(function(){
+		var pass = $('[name="password"]').val()
+		var cpass = $('[name="cpass"]').val()
+		if(cpass == '' ||pass == ''){
+			$('#pass_match').attr('data-status','')
+		}else{
+			if(cpass == pass){
+				$('#pass_match').attr('data-status','1').html('<i class="text-success">Password Matched.</i>')
+			}else{
+				$('#pass_match').attr('data-status','2').html('<i class="text-danger">Password does not match.</i>')
+			}
+		}
+	})
+	function displayImg(input,_this) {
+	    if (input.files && input.files[0]) {
+	        var reader = new FileReader();
+	        reader.onload = function (e) {
+	        	$('#cimg').attr('src', e.target.result);
+	        }
 
-<div class="container">
-<div class="row">
-<div class="col-lg-8 col-offset-2">
-<div class="page-header">
-<h2>Student Registration</h2>
-</div>
-<p>Get registered to submit your research papers</p>
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-<div class="form-group">
-<label>Name</label>
-<input type="text" name="name" class="form-control" value="" maxlength="50" required="">
-<span class="text-danger"><?php if (isset($name_error)) echo $name_error; ?></span>
-</div>
-<div class="form-group">
-<label>RegNo</label>
-<input type="text" name="regno" class="form-control" value="" maxlength="50" required="">
-<span class="text-danger"><?php if (isset($regno_error)) echo $regno_error; ?></span>
-</div>
-<div class="form-group">
-<label>Program</label>
-<input type="text" name="program" class="form-control" value="" maxlength="50" required="">
-<span class="text-danger"><?php if (isset($program_error)) echo $program_error; ?></span>
-</div>
-<div class="form-group ">
-<label>Email</label>
-<input type="email" name="email" class="form-control" value="" maxlength="30" required="">
-<span class="text-danger"><?php if (isset($email_error)) echo $email_error; ?></span>
-</div>
-<div class="form-group">
-<label>Mobile</label>
-<input type="text" name="mobile" class="form-control" value="" maxlength="12" required="">
-<span class="text-danger"><?php if (isset($mobile_error)) echo $mobile_error; ?></span>
-</div>
-<div class="form-group">
-<label>Password</label>
-<input type="password" name="password" class="form-control" value="" maxlength="8" required="">
-<span class="text-danger"><?php if (isset($password_error)) echo $password_error; ?></span>
-</div>  
-<div class="form-group">
-<label>Confirm Password</label>
-<input type="password" name="cpassword" class="form-control" value="" maxlength="8" required="">
-<span class="text-danger"><?php if (isset($cpassword_error)) echo $cpassword_error; ?></span>
-</div>
-
-
-<input type="submit" class="btn btn-primary" name="signup" value="submit">
-Already have a account?<a href="login.php" class="btn btn-default">Login</a>
-</form>
-</div>
-</div>    
-</div>
-</body>
-</html>
+	        reader.readAsDataURL(input.files[0]);
+	    }
+	}
+	$('#manage_user').submit(function(e){
+		e.preventDefault()
+		$('input').removeClass("border-danger")
+		start_load()
+		$('#msg').html('')
+		if($('[name="password"]').val() != '' && $('[name="cpass"]').val() != ''){
+			if($('#pass_match').attr('data-status') != 1){
+				if($("[name='password']").val() !=''){
+					$('[name="password"],[name="cpass"]').addClass("border-danger")
+					end_load()
+					return false;
+				}
+			}
+		}
+		$.ajax({
+			url:'ajax.php?action=save_user',
+			data: new FormData($(this)[0]),
+		    cache: false,
+		    contentType: false,
+		    processData: false,
+		    method: 'POST',
+		    type: 'POST',
+			success:function(resp){
+				if(resp == 1){
+					alert_toast('Data successfully saved.',"success");
+					setTimeout(function(){
+						location.replace('index.php?page=user_list')
+					},750)
+				}else if(resp == 2){
+					$('#msg').html("<div class='alert alert-danger'>Email already exist.</div>");
+					$('[name="email"]').addClass("border-danger")
+					end_load()
+				}
+			}
+		})
+	})
+</script>
